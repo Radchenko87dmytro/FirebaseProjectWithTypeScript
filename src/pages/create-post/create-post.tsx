5cdf0cc94918af428751778c7c34cc4cffe8dbd9
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2023-08-05 16:33:27 +0200

    Maiking navigate and creating post functionality

---
+++ src/pages/create-post/create-post.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Createform from './form'
+//import "../App.css"
+
+const CreatePost = () => {
+  return (
+    <div className="create-post">
+        <Createform />   
+    </div>
+ )
+}
+
+export default CreatePost
